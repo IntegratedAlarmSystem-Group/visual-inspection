ada89e745d08bfe278bcb853442013698ac350d4
Jazmine <jamaldonadof@ug.uchile.cl>
2018-09-27 19:17:09 -0300

    Configuration to run in production and using docker

--- visual_inspection/settings.py
+++ visual_inspection/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/visual-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Filename to registry the visual inspections
-VI_FILENAME = "inspections.json"
+VI_FILENAME = "output/inspections.json"
