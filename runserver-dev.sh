1e2c746705ec6ba0ffd32fbbae62b2aea03c10af
Sebastian <sebafehlandt@gmail.com>
2018-10-03 16:18:48 -0300

    Add Dockerfile-dev

---
+++ runserver-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# while ! nc -z postgres 5432
+# do
+#  echo "sleeping 1 second waiting for postgres"
+#  sleep 1
+# done
+
+# echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@fake-admin.com', 'nimda') if (User.objects.filter(username='admin').exists() == False) else None" | python manage.py shell
+echo "Running with development server!!"
+python manage.py runserver 0.0.0.0:8001
